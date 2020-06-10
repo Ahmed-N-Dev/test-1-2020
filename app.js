27bf3858e7b5ab3e622c6edc183d4d2135efb8cd
Ahmed-N-Dev <66719935+Ahmed-N-Dev@users.noreply.github.com>
2020-06-10 16:57:30 +0500

    Server Started on Port 3000

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Welcome");
+})
+
+
+app.listen(3000, function(){
+console.log("Server Started on Port 3000);
+});
